cfb5caee0c5e86e5a4c92d5dccdf3a2cc6c5a947
Sachin More <smore@infinidat.com>
2016-06-21 19:53:15 -0400

    Added signal handling code. Added code to open the device and determine its size and block size

--- siod.c
+++ siod.c
@@ -4,12 +4,22 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <scsi/sg_cmds.h>
+#include <scsi/sg_lib.h>
+#include <scsi/sg_io_linux.h>
+#include <signal.h>
 
 #include <string>
+
+#include "lfsr.h"
 
 static void print_usage(const std::string p) {
 	fprintf(stderr, "Usage:\n\n%s [rw] [rs] [0-9]+ [0-9]+ [0123] /dev/sg[0-9]+ <logfile>\n", p.c_str());
 	exit(-2);
+}
+
+static void getout(int s) {
+	exit(-1);
 }
 
 static std::string strtime() {
@@ -26,8 +36,13 @@ static std::string strtime() {
  * 0  : Normal Termination
  * -1 : caught SIGTERM
  * -2 : error in command line parameters
+ * -3 : error accessing the device
  */
 int main(int argc, char **argv) {
+	if (SIG_ERR == signal(SIGINT, getout)) {
+		fprintf(stderr, "Cannot manage SIGINT: %s\n", strerror(errno));
+		return -1;
+	}
 	if (argc != 8) {
 		print_usage(argv[0]);
 	}
@@ -92,6 +107,31 @@ int main(int argc, char **argv) {
 	if (logfp) fprintf(logfp, "%s UTC: %s %c %c %hu %hu %hu %s %s\n", strtime().c_str(), argv[0], argv[1][0], argv[2][0], iosize, qdepth, key, device.c_str(), logfilename.c_str());
 
 	uint64_t blocks_accessed = 0;
+
+	const int fd = sg_cmds_open_device(device.c_str(), 0, 0);
+	if (fd < 0) {
+		fprintf(stderr, "Error opening device %s: %s\n", device.c_str(), strerror(errno));
+		return -3;
+       	}
+
+	uint64_t max_lba = 0;
+       	uint32_t blocksize = 0;
+       	{
+			unsigned char resp[32];
+			bzero(resp, 32);
+			if (0 != sg_ll_readcap_16(fd, 0, 0, resp, 32, 0, 0)) {
+			       	fprintf(stderr, "Read capacity failed on device %s: %s\n", device.c_str(), strerror(errno));
+				return -3;
+			}
+			for (unsigned int i = 0; i < 8; i++) {
+				max_lba = (max_lba << 8) + resp[i];
+			}
+			for (unsigned int i = 8; i < 12; i++) {
+				blocksize = (blocksize << 8) + resp[i];
+			}
+       	}
+	if (logfp) fprintf(logfp, "%s UTC: Max LBA %lu Block Size %u\n", strtime().c_str(), max_lba, blocksize);
+	sg_cmds_close_device(fd);
 
 	if (logfp) fprintf(logfp, "%s UTC: %lu blocks accessed\n", strtime().c_str(), blocks_accessed);
 
