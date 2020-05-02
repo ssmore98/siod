3f24521f2e743b288f1291b3562dea5a25719d3d
smore <sachinsureshmore@gmail.com>
2020-05-02 15:07:09 -0400

    changes to accomodate siod-common.c

--- siod.c
+++ siod.c
@@ -39,10 +39,6 @@
 #include "lfsr.h"
 #include "siod.h"
 
-#define CDB_SIZE     ((uint16_t)16)
-#define SENSE_LENGTH ((uint16_t)32)
-#define STATUS_BLKCNT ((uint64_t)2 * 1024 * 1024)
-
 typedef struct tagIO {
     bool used;
     uint16_t me;
@@ -57,36 +53,6 @@ static uint64_t blocks_accessed = 0;
 static bool     data_miscompare = false;
 static uint64_t data_key_count[4];
 
-typedef enum {ErrorNone, ErrorSyscall, ErrorIO, ErrorData, ErrorBlock,
-    ErrorSignal, ErrorInternal} ErrorType;
-
-static std::string strtime() {
-    time_t t;
-    if (0 > time(&t)) {
-        return std::string();
-    }
-    struct tm * mytm = gmtime(&t);
-    if (NULL == mytm) {
-        return std::string();
-    }
-    const char * const ts = asctime(mytm);
-    if (NULL == ts) {
-        return std::string();
-    }
-    std::string retval(ts);
-    retval.pop_back();
-    return retval;
-}
-
-static std::string cdb2str(const unsigned char * const cdb);
-static std::string sense2str(const unsigned char * const sense);
-
-static std::string UINT64(const uint64_t & x) {
-    std::ostringstream o;
-    o << "0x" << std::setfill('0') << std::hex << std::setw(16) << x;
-    return o.str();
-}
-
 static std::string KeyCounts() {
     std::ostringstream o;
     o << UINT64(data_key_count[0]) << ' ';
@@ -96,11 +62,6 @@ static std::string KeyCounts() {
     return o.str();
 }
 
-static std::string LoglineStart() {
-    std::ostringstream o;
-           o << strtime() << " UTC:";
-    return o.str();
-}
 
 static bool DeleteSHM(const key_t & shmkey) {
     // delete the shared memory segment to track slave progress
@@ -149,11 +110,11 @@ possible during buffer flush\n",
     exit(status);
 }
 
-static void logprint(const char * const file, const int & line,
+void logprint(const char * const file, const int & line,
         const ErrorType & e, const bool & fatal,
-        const std::string & s = std::string(), const double & start = 0,
-        const double & end = 0, const unsigned char * const cdb = NULL,
-        const unsigned char * const sbp = NULL) {
+        const std::string & s, const double & start,
+        const double & end, const unsigned char * const cdb,
+        const unsigned char * const sbp) {
     std::ostringstream head;
     head << LoglineStart() << file << ':' << line << ": ";
     switch (e) {
@@ -213,28 +174,6 @@ static void logprint(const char * const file, const int & line,
                     head.str().c_str());
             break;
     }
-}
-
-static std::string buffer2str(const unsigned char * const buf,
-        const uint16_t & sz) {
-    if (!buf || !sz) return std::string();
-    std::string retval;
-    for (uint16_t i = 0; i < sz; i++) {
-        char str[6];
-        if (0 > snprintf(str, 6, "0x%02X ", ((uint8_t)buf[i]))) {
-            logprint(__FILE__, __LINE__, ErrorSyscall, true, "snprintf");
-        }
-        retval += str;
-    }
-    return retval;
-}
-
-static std::string cdb2str(const unsigned char * const cdb) {
-    return buffer2str(cdb, CDB_SIZE);
-}
-
-static std::string sense2str(const unsigned char * const sense) {
-    return buffer2str(sense, SENSE_LENGTH);
 }
 
 static double time2double(const struct timeval & t) {
--- siod.h
+++ siod.h
@@ -1,4 +1,28 @@
 #ifndef SIOD_H
 #define SIOD_H
+
+#include <string>
+
+#define CDB_SIZE     ((uint16_t)16)
+#define SENSE_LENGTH ((uint16_t)32)
+#define STATUS_BLKCNT ((uint64_t)2 * 1024 * 1024)
+
+typedef enum {ErrorNone, ErrorSyscall, ErrorIO, ErrorData, ErrorBlock, ErrorSignal, ErrorInternal}
+ErrorType;
+
 static const char * const lockfile = "/tmp/siod.lck";
+
+void logprint(const char * const file, const int & line,
+        const ErrorType & e, const bool & fatal,
+        const std::string & s = std::string(), const double & start = 0,
+        const double & end = 0, const unsigned char * const cdb = NULL,
+        const unsigned char * const sbp = NULL);
+std::string buffer2str(const unsigned char * const buf, const uint16_t & sz);
+std::string cdb2str(const unsigned char * const cdb);
+std::string sense2str(const unsigned char * const sense);
+std::string LoglineStart();
+std::string UINT64(const uint64_t & x);
+std::string strtime();
+
+
 #endif
